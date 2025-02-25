69c4f5cf92aa47d79c5912703b6160ef496a88a5
Catbjorhan <37435377+catbjor@users.noreply.github.com>
2025-02-25 13:06:00 +0100

    Add files via upload

---
+++ number_guessing_game.py
@@ -0,0 +1,25 @@
+import random
+lower = int(input("Enter the lower bound: "))
+upper = int(input("\nEnter the upper bound: "))
+if lower >= upper:
+    print('\nUpper bound must be greater that lower bound')
+    exit()
+num = random.randint(lower, upper)
+chances = 3
+print(f"\nYou have {chances} chances to guess the number!\n")
+count = 0
+guessed = False
+while count < chances:
+    count += 1
+    guess = int(input("Guess a number: "))
+    if num == guess:
+        print("Congratulations, you did it in ", count, "try!")
+        guessed = True
+        break
+    elif num > guess:
+        print("You guessed too small.")
+    elif num < guess:
+        print("You guessed too high.")
+if not guessed:
+    print("\nThe number is %d" % num)
+    print("\nBetter luck next time!")
